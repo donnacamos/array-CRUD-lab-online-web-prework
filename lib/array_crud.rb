3889989dee798b9e34ee0a3aa919202c6822298e
Donna Amos <donnacamos.developer@gmail.com>
2019-02-19 02:50:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,11 +17,11 @@ def add_element_to_start_of_array(array = [ "I", "am", "really", "learning", "ar
 end
 
 def remove_element_from_end_of_array(array = ["wow", "I", "am", "really", "learning"], element = ["arrays"])
-  
+  array.pop(element)
 end
 
 def remove_element_from_start_of_array(array = [ "I", "am", "really", "learning", "arrays"], element = ["wow"])
-  
+  array.shift(element)
 end
 
 def retrieve_element_from_index(array, index_number)
