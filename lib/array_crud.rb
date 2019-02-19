26af034cc046824c23a3419cb968f37ca9523f27
Donna Amos <donnacamos.developer@gmail.com>
2019-02-19 03:04:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -25,7 +25,7 @@ def remove_element_from_start_of_array(array = ["wow", "I", "am", "really", "lea
 end
 
 def retrieve_element_from_index(array = ["wow", "I", "am", "really", "learning", "arrays"], index_number = [2])
-  
+  array
 end
 
 def retrieve_first_element_from_array(array)
