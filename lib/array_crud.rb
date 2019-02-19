c85fdf9f07cf10f8e0a432fd4e4aab80f7b58e9c
Donna Amos <donnacamos.developer@gmail.com>
2019-02-19 03:08:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,6 +32,6 @@ def retrieve_first_element_from_array(array = ["wow", "I", "am", "really", "lear
   array[0]
 end
 
-def retrieve_last_element_from_array(array)
-  
+def retrieve_last_element_from_array(array = ["wow", "I", "am", "really", "learning", "arrays"])
+  array[5]
 end
