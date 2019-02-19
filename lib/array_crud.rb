a8a9924373a0853c058d9d2918e6d2e717bbdab3
Donna Amos <donnacamos.developer@gmail.com>
2019-02-19 02:37:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = [0, 1, 2, 3]
+   array = ["wow", "I", "am", "really", "learning"]
+   element = ["arrays!"]
   array.push(element)
   puts array.inspect
 end
